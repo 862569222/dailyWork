e5009b2c6b4487ec59aef6a685def7bf37943672
zhaibo <862569222@qq.com>
2020-08-02 21:07:23 +0800

    daily test

---
+++ src/test/java/daily/Strings/HashMapTest.java
@@ -0,0 +1,15 @@
+package daily.Strings;
+
+import com.sun.javafx.collections.MappingChange;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapTest {
+    public static void main(String[] args) {
+        Map map = new HashMap();
+        Map map2 = new HashMap(map);
+
+
+    }
+}
---
+++ src/test/java/daily/Strings/Strings.java
@@ -0,0 +1,25 @@
+package daily.Strings;
+
+
+import org.junit.jupiter.api.Test;
+
+public class Strings {
+
+
+    @Test
+    public void strings(){
+        String a ="zhaibo";//这个引用直接指向的是常量池中的地址
+        String b = new String("zhaibo");//这种方式是在堆中创建一个对象 对象中存储了指向这个字符串在常量池中的地址
+        String c = new String(a).intern();
+        System.out.println(a==c);
+        System.out.println(a==b);
+    }
+
+    @Test
+    public void length(){
+        int[][] a = new int[2][];
+        int[][] b = new int[5][2];
+        System.out.println(a.getClass());
+        System.out.println(b.length);
+    }
+}
